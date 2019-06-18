59cc416240ddf4e17fb953945d11412999ce0131
Tyson Roehrkasse <tysonr77@gmail.com>
2019-06-17 23:32:20 -0600

    additional package tracking features and update dependencies

--- framework/modules/packages/packages.php
+++ framework/modules/packages/packages.php
@@ -47,11 +47,19 @@ $appointable_products = wc_get_products($args);
 
 $select_options[''] = __( 'Select a value', 'woocommerce');
 foreach ($appointable_products as $product) {
+	$id = $product->get_id();
 	$title = $product->get_title();
-	$select_options[$title] = $title;
+	$select_options[$id] = $title;
 }
+//!Kint::dump(get_post_meta(get_the_ID())); die();
 ?><div id='appointment_package_options' class='panel woocommerce_options_panel'><?php
 	?><div class='options_group'><?php
+		woocommerce_wp_text_input( 
+			array(
+				'id'		=>	'_appointment_package_quantity',
+				'label'		=>	__( 'How many in package?', 'woocommerce' ),
+			)
+		);
 		woocommerce_wp_select( 
 			array( 
 				'id'      => '_appointment_package_type', 
@@ -59,19 +67,15 @@ foreach ($appointable_products as $product) {
 				'options' =>  $select_options
 				)
 			);
-			woocommerce_wp_text_input( 
-				array(
-					'id'		=>	'appointment_package_quantity',
-					'label'	=>	'How many in package?'
-				)
-				);
 	?></div>
 </div><?php
 
 }
 
 /* Customize the tabs that display for this product type */
-add_filter( 'woocommerce_product_tabs', 'appointment_package_edit_product_tabs', 98 );
+if (is_admin()) {
+	add_filter( 'woocommerce_product_tabs', 'appointment_package_edit_product_tabs', 98 );
+}
 function appointment_package_edit_product_tabs ( $tabs ) {
 	array_push($tabs['general']['class'], 'show_if_appointment_package');
 	return $tabs;
@@ -84,6 +88,23 @@ function appointment_package_custom_js () {
   ?><script type='text/javascript'>
 		jQuery( '.options_group.pricing' ).addClass( 'show_if_appointment_package' );
   </script><?php
+}
+
+/* Save data in our new product fields */
+add_action( 'woocommerce_process_product_meta', 'save_appointment_package_options_field' );
+function save_appointment_package_options_field( $post_id ) {
+	
+	if ( isset( $_POST['_appointment_package_type'] ) ) :
+		update_post_meta( $post_id, '_appointment_package_type', sanitize_text_field( $_POST['_appointment_package_type'] ) );
+	endif;
+
+	if ( isset( $_POST['_appointment_package_quantity'] ) ) :
+		update_post_meta( $post_id, '_appointment_package_quantity', sanitize_text_field( $_POST['_appointment_package_quantity'] ) );
+	endif;
+	
+	if ( isset( $_POST['_appointment_package_price'] ) ) :
+		update_post_meta( $post_id, '_appointment_package_price', sanitize_text_field( $_POST['_appointment_package_price'] ) );
+	endif;
 }
 
 /* Add custom menu to Wordpress Admin for package tracking */
@@ -105,4 +126,36 @@ function render_appointments_plus_admin_menu(){
 		 'info'      =>  'Details for packages and the ability to add new ones will show here.'
 	];
 	Timber::render('package-tracking.twig', $context);
+}
+
+/* Add the "Add to Cart" button on appointment package pages */
+function appointment_package_add_to_cart_button() {
+    wc_get_template( 'single-product/add-to-cart/simple.php' );
+}
+add_action( 'woocommerce_appointment_package_add_to_cart', 'appointment_package_add_to_cart_button' );
+
+/* 
+ * Customize the "Added to Cart" message for Packages
+ * If an appointment package is added to the cart we want the user to schedule an appointment before checking out
+ */
+add_filter( 'wc_add_to_cart_message_html', 'appointment_packages_add_to_cart_function', 10, 2 ); 
+function appointment_packages_add_to_cart_function( $message, $products ) {
+	$purchased = intval(key($products));
+	// Get a list of appointment package products
+	$args = [
+		'type'		=>	'appointment_package'
+	];
+	$appointment_packages = wc_get_products($args);
+
+	// See if the purchased item matches an appointment package product
+	foreach ($appointment_packages as $package) {
+		if ($package->get_id() == $purchased) {
+			$appointment = intval(get_post_meta($purchased, '_appointment_package_type', true));
+			// Match! Customize the message accordingly
+			$message = '<a href="' . get_permalink($appointment) . '" tabindex="1" class="button wc-forward">Schedule Appointment</a>' . 
+			get_the_title(key($products)) . 
+			' has been added to your cart. Please schedule your first appointment before checking out!';
+		}
+	}
+	return $message; 
 }
